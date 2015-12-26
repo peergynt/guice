0ace994eeffd69c913845980d2e0deb4d3ca7eca
Bernd Hopp <berndjhopp@gmail.com>
2015-12-26 16:29:26 +0100

    add DefaultView-Annotation

--- vaadin-guice/src/main/java/com/vaadin/guice/server/GuiceUIProvider.java
+++ vaadin-guice/src/main/java/com/vaadin/guice/server/GuiceUIProvider.java
@@ -15,7 +15,11 @@
  */
 package com.vaadin.guice.server;
 
+import com.vaadin.guice.annotation.DefaultView;
 import com.vaadin.guice.annotation.GuiceUI;
+import com.vaadin.navigator.Navigator;
+import com.vaadin.navigator.ViewDisplay;
+import com.vaadin.navigator.ViewProvider;
 import com.vaadin.server.ServiceException;
 import com.vaadin.server.SessionInitEvent;
 import com.vaadin.server.SessionInitListener;
@@ -23,17 +27,21 @@ import com.vaadin.server.UIClassSelectionEvent;
 import com.vaadin.server.UICreateEvent;
 import com.vaadin.server.UIProvider;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.ComponentContainer;
+import com.vaadin.ui.SingleComponentContainer;
 import com.vaadin.ui.UI;
 import com.vaadin.util.CurrentInstance;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Field;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Vaadin {@link com.vaadin.server.UIProvider} that looks up UI classes from the Guice application
@@ -49,6 +57,7 @@ class GuiceUIProvider extends UIProvider implements SessionInitListener {
 
     private final Map<String, Class<? extends UI>> pathToUIMap = new ConcurrentHashMap<String, Class<? extends UI>>();
     private final Map<String, Class<? extends UI>> wildcardPathToUIMap = new ConcurrentHashMap<String, Class<? extends UI>>();
+    private final Map<Class<? extends UI>, Field> uiToDefaultViewField = new ConcurrentHashMap<Class<? extends UI>, Field>();
 
     public GuiceUIProvider(Set<Class<?>> uiClasses) {
         detectUIs(uiClasses);
@@ -62,11 +71,16 @@ class GuiceUIProvider extends UIProvider implements SessionInitListener {
     protected void detectUIs(Set<Class<?>> uiClasses) {
         logger.info("Checking the application context for Vaadin UIs");
 
-        for (Class<?> uiClass : uiClasses) {
-            checkArgument(UI.class.isAssignableFrom(uiClass), "class %s has GuiceUI annotation but is not of type com.vaadin.ui.UI.", uiClass);
+        for (Class<?> uiClassRaw : uiClasses) {
+            checkArgument(UI.class.isAssignableFrom(uiClassRaw), "class %s has GuiceUI annotation but is not of type com.vaadin.ui.UI.", uiClassRaw);
+
+            Class<? extends UI> uiClass = (Class<? extends UI>) uiClassRaw;
 
             logger.info("Found Vaadin UI [{}]", uiClass.getCanonicalName());
-            String path = uiClass.getAnnotation(GuiceUI.class).path();
+
+            GuiceUI annotation = uiClass.getAnnotation(GuiceUI.class);
+
+            String path = annotation.path();
             path = preparePath(path);
 
             Class<? extends UI> existingUiForPath = getUIByPath(path);
@@ -77,8 +91,31 @@ class GuiceUIProvider extends UIProvider implements SessionInitListener {
             }
             logger.debug("Mapping Vaadin UI [{}] to path [{}]",
                     uiClass.getCanonicalName(), path);
-            mapPathToUI(path, (Class<? extends UI>) uiClass);
+            mapPathToUI(path, uiClass);
+            mapToDefaultViewField(uiClass);
         }
+    }
+
+    private void mapToDefaultViewField(Class<? extends UI> uiClass) {
+
+        Field defaultViewField = null;
+
+        for (java.lang.reflect.Field field : uiClass.getDeclaredFields()) {
+            if(field.getAnnotation(DefaultView.class) == null){
+                continue;
+            }
+
+            checkArgument(defaultViewField == null, "more than one field annotated with @DefaultView in class " + uiClass);
+
+            defaultViewField = field;
+        }
+
+        if(defaultViewField == null){
+            return;
+        }
+
+        defaultViewField.setAccessible(true);
+        uiToDefaultViewField.put(uiClass, defaultViewField);
     }
 
     private String preparePath(String path) {
@@ -148,7 +185,39 @@ class GuiceUIProvider extends UIProvider implements SessionInitListener {
         CurrentInstance.set(key, identifier);
 
         try {
-            return InjectorHolder.getInjector().getInstance(event.getUIClass());
+            UI instance = InjectorHolder.getInjector().getInstance(event.getUIClass());
+
+            Field defaultViewField = uiToDefaultViewField.get(event.getUIClass());
+
+            if(defaultViewField != null){
+
+                Object defaultView;
+
+                try {
+                     defaultView = defaultViewField.get(instance);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+
+                checkNotNull(defaultView, "field %s in %s is annotated with @DefaultUI and therefore must not be null");
+
+                Navigator navigator;
+
+                if(defaultView instanceof ComponentContainer){
+                    navigator = new Navigator(instance, (ComponentContainer) defaultView);
+                } else if (defaultView instanceof SingleComponentContainer) {
+                    navigator = new Navigator(instance, (SingleComponentContainer) defaultView);
+                } else if (defaultView instanceof ViewDisplay) {
+                    navigator = new Navigator(instance, (ViewDisplay) defaultView);
+                } else {
+                    throw new IllegalArgumentException("%s.%s is annotated with @DefaultUI, must be either ComponentContainer, SingleComponentContainer or ViewDisplay");
+                }
+
+                navigator.addProvider(InjectorHolder.getInjector().getInstance(ViewProvider.class));
+                instance.setNavigator(navigator);
+            }
+
+            return instance;
         } finally {
             CurrentInstance.set(key, null);
         }
