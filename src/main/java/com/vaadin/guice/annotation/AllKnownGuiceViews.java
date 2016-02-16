167eba3e2040846bdc555ceecb01702f3ef0841e
berndhopp <bernd@vaadin.com>
2016-02-16 14:40:40 +0100

    added feature to inject all known guice-views in set

---
+++ src/main/java/com/vaadin/guice/annotation/AllKnownGuiceViews.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2015 The original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.vaadin.guice.annotation;
+
+import com.google.inject.BindingAnnotation;
+import com.google.inject.ScopeAnnotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to be placed on a Set of {@link com.vaadin.navigator.View}s that should contain
+ * all known Views annotated with {@link GuiceView}
+ *
+ * &#064;GuiceView(name = &quot;myView&quot;, ui = MyUI.class)
+ * public class MyView extends CustomComponent implements View {
+ *     // ...
+ * }
+ * </pre>
+ *
+ * {@literal @}Inject
+ * {@literal @}AllKnownGuiceViews
+ * private Set&lt;View&gt; guiceViews;
+ *
+ * @author Petter Holmström (petter@vaadin.com)
+ * @author Henri Sara (hesara@vaadin.com)
+ * @author Bernd Hopp (bernd@vaadin.com)
+ */
+@Target({ElementType.FIELD})
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Documented
+@BindingAnnotation
+public @interface AllKnownGuiceViews {
+}
