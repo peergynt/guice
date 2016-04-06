37b3770498e76a322154fa9c5ce312bce7bb9c06
berndhopp <bernd@vaadin.com>
2016-04-06 13:54:17 +0200

    Little refactorings for GlobaleEventBus

--- src/main/java/com/vaadin/guice/bus/GlobalEventBus.java
+++ src/main/java/com/vaadin/guice/bus/GlobalEventBus.java
@@ -16,38 +16,53 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+
 @Singleton
-public class GlobalEventBus extends EventBus implements SessionDestroyListener, SessionInitListener {
+@SuppressWarnings("unused")
+public class GlobalEventBus extends EventBus {
 
     private final Map<VaadinSession, Set<Object>> registeredObjectsBySession = new HashMap<VaadinSession, Set<Object>>();
 
     GlobalEventBus() {
-        VaadinService.getCurrent().addSessionInitListener(this);
-        VaadinService.getCurrent().addSessionDestroyListener(this);
+        VaadinService.getCurrent().addSessionInitListener(new SessionInitListener() {
+            @Override
+            public void sessionInit(SessionInitEvent sessionInitEvent) throws ServiceException {
+                registeredObjectsBySession.put(sessionInitEvent.getSession(), new HashSet<Object>());
+            }
+        });
+
+        VaadinService.getCurrent().addSessionDestroyListener(new SessionDestroyListener() {
+            @Override
+            public void sessionDestroy(SessionDestroyEvent sessionDestroyEvent) {
+                releaseAll(sessionDestroyEvent.getSession());
+            }
+        });
+    }
+
+    private void releaseAll(VaadinSession vaadinSession){
+        Set<Object> registeredObjects = registeredObjectsBySession.remove(vaadinSession);
+
+        checkState(registeredObjects != null);
+
+        for (Object registeredObject : registeredObjects) {
+            super.unregister(registeredObject);
+        }
     }
 
     @Override
     public void register(Object object) {
+        checkNotNull(object);
         registeredObjectsBySession.get(VaadinSession.getCurrent()).add(object);
         super.register(object);
     }
 
     @Override
     public void unregister(Object object) {
+        checkNotNull(object);
         registeredObjectsBySession.get(VaadinSession.getCurrent()).remove(object);
         super.unregister(object);
-    }
-
-    @Override
-    public void sessionDestroy(SessionDestroyEvent event) {
-        for (Object registeredObject : registeredObjectsBySession.remove(event.getSession())) {
-            super.unregister(registeredObject);
-        }
-    }
-
-    @Override
-    public void sessionInit(SessionInitEvent event) throws ServiceException {
-        registeredObjectsBySession.put(event.getSession(), new HashSet<Object>());
     }
 }
 
