d5fab86d86c65b4fe5e6efdd0ea7965e3d2598dd
berndhopp <bernd@vaadin.com>
2016-10-14 13:59:44 +0200

    GuiceNavigator doc

--- src/main/java/com/vaadin/guice/server/GuiceNavigator.java
+++ src/main/java/com/vaadin/guice/server/GuiceNavigator.java
@@ -1,14 +1,17 @@
 package com.vaadin.guice.server;
 
+import com.vaadin.guice.annotation.GuiceUI;
 import com.vaadin.navigator.Navigator;
 import com.vaadin.navigator.ViewDisplay;
 import com.vaadin.ui.ComponentContainer;
 import com.vaadin.ui.SingleComponentContainer;
 import com.vaadin.ui.UI;
 
+/**
+ * subclass GuiceNavigator if you need a custom navigator. To use,
+ * set your GuiceNavigator's class in {@link GuiceUI#navigator()}
+ */
 public class GuiceNavigator extends Navigator {
-
-    private static final long serialVersionUID = -2693174603614667628L;
 
     void init(UI ui, ComponentContainer container) {
         init(ui, new ComponentContainerViewDisplay(container));
