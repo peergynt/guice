bc9254423f7693991214e473215fef5e96cc11f6
berndhopp <bernd@vaadin.com>
2016-10-14 13:21:44 +0200

    deprecate NavigatorProvider

--- src/main/java/com/vaadin/guice/server/NavigatorProvider.java
+++ src/main/java/com/vaadin/guice/server/NavigatorProvider.java
@@ -5,6 +5,7 @@ import com.google.inject.Provider;
 import com.vaadin.navigator.Navigator;
 import com.vaadin.ui.UI;
 
+@Deprecated //use UI.getCurrent().getNavigator()
 public class NavigatorProvider implements Provider<Navigator> {
     @Override
     public Navigator get() {
